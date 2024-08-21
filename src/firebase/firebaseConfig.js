c416c39e6f18fcb0f1980c65cd77ce6c1bbb9ce2
Bruno Wuo <104470819+BrunoWuo@users.noreply.github.com>
2024-08-20 22:46:17 -0300

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,12 +5,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkXT1NKQ8-iRk5SDGDXcy936yzKOw8cqc",
-  authDomain: "fir-app-f803f.firebaseapp.com",
-  projectId: "fir-app-f803f",
-  storageBucket: "fir-app-f803f.appspot.com",
-  messagingSenderId: "883262263482",
-  appId: "1:883262263482:web:7b113b312e4aa7c0cbf283"
+ 
 };
 
 // Initialize Firebase
